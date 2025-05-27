f68a092c6ef019c95a25297335b8ddf6baa5d4f9
Hritik Raj <hritik23154049@akgec.ac.in>
2025-05-27 23:45:16 +0530

    JWT token added

---
+++ GDG_Community_Blog_Platform/src/main/java/com/GDGSocialMedia/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.GDGSocialMedia.controller;
+
+
+import com.GDGSocialMedia.config.JwtProvider;
+import com.GDGSocialMedia.models.User;
+import com.GDGSocialMedia.repository.UserRepository;
+import com.GDGSocialMedia.response.AuthResponse;
+import com.GDGSocialMedia.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+     @Autowired
+     private UserService userService;
+
+     @Autowired
+     private UserRepository userRepository;
+
+     @Autowired
+     private PasswordEncoder passwordEncoder;
+
+     @PostMapping("/signup")
+     public AuthResponse createUser(@RequestBody User user) throws Exception {
+         User isExist =userRepository.findByEmail(user.getEmail());
+         if(isExist!=null){
+             throw new Exception("this email already exist");
+         }
+
+         User newUser= new User();
+
+         newUser.setEmail(user.getEmail());
+         newUser.setFirstName(user.getFirstName());
+         newUser.setLastName(user.getLastName());
+         newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+         User savedUser=userRepository.save(newUser);
+
+         Authentication authentication=new UsernamePasswordAuthenticationToken(savedUser.getEmail(),savedUser.getPassword());
+         String token = JwtProvider.generateToken(authentication);
+         AuthResponse res=new AuthResponse(token, "Resgister Success");
+
+         return res;
+     }
+}
