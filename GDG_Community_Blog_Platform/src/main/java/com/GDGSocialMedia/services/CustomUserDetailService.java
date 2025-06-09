eb1bc006fe79824879420c55a033af4817573ea7
Hritik Raj <hritik23154049@akgec.ac.in>
2025-06-09 20:25:57 +0530

    All working

--- GDG_Community_Blog_Platform/src/main/java/com/GDGSocialMedia/services/CustomUserDetailService.java
+++ GDG_Community_Blog_Platform/src/main/java/com/GDGSocialMedia/services/CustomUserDetailService.java
@@ -18,15 +18,13 @@ public class CustomUserDetailService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-        User user=userRepository.findByEmail(username);
-        if(user==null){
-            throw new UsernameNotFoundException("User not found with "+ user.getEmail());
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + username);
         }
 
-        List<GrantedAuthority> authorities=new ArrayList<>();
-
-
+        List<GrantedAuthority> authorities = new ArrayList<>();
 
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
